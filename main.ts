3f691c419e18a0e5be4ae5ffbc24feb0fec3637e
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-11-02 18:38:05 -0500

    redone

--- main.ts
+++ main.ts
@@ -2,59 +2,59 @@
  *
  * Created by: Tiago Baker
  * Created on: Oct 2025
- * This program lights up single LED
- * Created by: Tiago baker
- * Created on: oct 2020
- * This program ...
+ * This program lights up single LED 
 */
 
-basic.clearScreen()
-basic.showIcon(IconNames.Happy)
-
-//input.onButtonPressed(Button.A,function () {
-    //pins.digitalWritePin(DigitalPin.P13,
-//
 // Define RGB LED pins
 let red = DigitalPin.P13
 let green = DigitalPin.P14
 let blue = DigitalPin.P15
 
-// Helper function to set color
-function setColor(r: number, g: number, b: number) {
-    pins.digitalWritePin(red, r)
-    pins.digitalWritePin(green, g)
-    pins.digitalWritePin(blue, b)
-}
-
 input.onButtonPressed(Button.A, function () {
     // Red
-    setColor(1, 0, 0)
+    pins.digitalWritePin(red, 1)
+    pins.digitalWritePin(green, 0)
+    pins.digitalWritePin(blue, 0)
     basic.pause(1000)
 
     // Green
-    setColor(0, 1, 0)
+    pins.digitalWritePin(red, 0)
+    pins.digitalWritePin(green, 1)
+    pins.digitalWritePin(blue, 0)
     basic.pause(1000)
 
     // Blue
-    setColor(0, 0, 1)
+    pins.digitalWritePin(red, 0)
+    pins.digitalWritePin(green, 0)
+    pins.digitalWritePin(blue, 1)
     basic.pause(1000)
 
     // Yellow (Red + Green)
-    setColor(1, 1, 0)
+    pins.digitalWritePin(red, 1)
+    pins.digitalWritePin(green, 1)
+    pins.digitalWritePin(blue, 0)
     basic.pause(1000)
 
     // Cyan (Green + Blue)
-    setColor(0, 1, 1)
+    pins.digitalWritePin(red, 0)
+    pins.digitalWritePin(green, 1)
+    pins.digitalWritePin(blue, 1)
     basic.pause(1000)
 
     // Magenta (Red + Blue)
-    setColor(1, 0, 1)
+    pins.digitalWritePin(red, 1)
+    pins.digitalWritePin(green, 0)
+    pins.digitalWritePin(blue, 1)
     basic.pause(1000)
 
-    // White (Red + Green + Blue)
-    setColor(1, 1, 1)
+    // White (All on)
+    pins.digitalWritePin(red, 1)
+    pins.digitalWritePin(green, 1)
+    pins.digitalWritePin(blue, 1)
     basic.pause(1000)
 
-    // Turn off LED
-    setColor(0, 0, 0)
+    // Turn all off
+    pins.digitalWritePin(red, 0)
+    pins.digitalWritePin(green, 0)
+    pins.digitalWritePin(blue, 0)
 })
