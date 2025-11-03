2a1119ec1f52b2949fcd63168348c7a7622f198d
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-11-02 19:12:04 -0500

    redone again and fixed the comments

--- main.ts
+++ main.ts
@@ -5,56 +5,59 @@
  * This program lights up single LED 
 */
 
-// Define RGB LED pins
-let red = DigitalPin.P13
-let green = DigitalPin.P14
-let blue = DigitalPin.P15
-
 input.onButtonPressed(Button.A, function () {
+
     // Red
-    pins.digitalWritePin(red, 1)
-    pins.digitalWritePin(green, 0)
-    pins.digitalWritePin(blue, 0)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
     basic.pause(1000)
 
     // Green
-    pins.digitalWritePin(red, 0)
-    pins.digitalWritePin(green, 1)
-    pins.digitalWritePin(blue, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
     basic.pause(1000)
+
 
     // Blue
-    pins.digitalWritePin(red, 0)
-    pins.digitalWritePin(green, 0)
-    pins.digitalWritePin(blue, 1)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
     basic.pause(1000)
+
 
     // Yellow (Red + Green)
-    pins.digitalWritePin(red, 1)
-    pins.digitalWritePin(green, 1)
-    pins.digitalWritePin(blue, 0)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
     basic.pause(1000)
 
+
     // Cyan (Green + Blue)
-    pins.digitalWritePin(red, 0)
-    pins.digitalWritePin(green, 1)
-    pins.digitalWritePin(blue, 1)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
     basic.pause(1000)
 
     // Magenta (Red + Blue)
-    pins.digitalWritePin(red, 1)
-    pins.digitalWritePin(green, 0)
-    pins.digitalWritePin(blue, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
     basic.pause(1000)
 
-    // White (All on)
-    pins.digitalWritePin(red, 1)
-    pins.digitalWritePin(green, 1)
-    pins.digitalWritePin(blue, 1)
+
+    // White (Red + Green + Blue)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
     basic.pause(1000)
 
-    // Turn all off
-    pins.digitalWritePin(red, 0)
-    pins.digitalWritePin(green, 0)
-    pins.digitalWritePin(blue, 0)
+
+    // Turn off LED
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.pause(1000)
+
 })
